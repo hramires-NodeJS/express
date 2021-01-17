d0fe5322cee82ac3f3f4a8c8766eaa3f92892360
Henrique <Ramires>
2021-01-16 23:59:50 -0300

    Adicionando msg ao startar

--- src/index.js
+++ src/index.js
@@ -6,4 +6,6 @@ app.get('/projects', (request, response) => {
     return response.json({ message: 'Bombou!' });
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+    console.log('😀 Back-end started!')
+});

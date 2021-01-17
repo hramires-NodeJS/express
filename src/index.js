9045729a06ab9903169cae628be6578614901211
Henrique <Ramires>
2021-01-17 00:03:08 -0300

    Métodos HTTP

--- src/index.js
+++ src/index.js
@@ -2,8 +2,45 @@ const express = require('express');
 
 const app = express();
 
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar informações do back-end
+ * POST: Criar uma informação no back-end
+ * PUT/PATH: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
 app.get('/projects', (request, response) => {
-    return response.json({ message: 'Bombou!' });
+    return response.json([
+        'Projeto 1',
+        'Projeto 2'
+    ]);
+});
+
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+// http://localhost:3333/projects/2
+
+app.put('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 4',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 2',
+        'Projeto 3'
+    ]);
 });
 
 app.listen(3333, () => {
